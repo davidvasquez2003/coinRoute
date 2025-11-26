402023dbee79cae0a7692a11f7a5fe6958a358ed
davidvasquez2003 <davidandresvasquez2003@gmail.com>
2025-11-25 22:25:23 -0500

    Added custom positive_decimal() validator to ensure --qty is a valid numeric value
    Make sure quantity is greater than zero
    
    Prevents crashes by invalid user input

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import time
 import requests
 import argparse
 
-
+# Limiter to calls to the api for the period of 2 seconds 
 def rate_limiter(period):
     def decorator(func):
         # mutable so inner function can be modified 
-        last_call=[0.0]
+        last_call=[0]
         
         def wrapper(*args, **kwargs):
             current_time = time.time()
@@ -36,17 +36,25 @@ def fetch_gemini ():
     
     # Gemini return values for bids/asks in the string "price" and "amount"
     for entry in data ["bids"]:
+        if "price"not in entry or "amount" not in entry:
+            print("Malformed Entry bids:", entry)
+            continue
+        
         price = Decimal(entry["price"])
         amount= Decimal(entry["amount"])
         bids.append([price, amount])
         
     for entry in data ["asks"]:
+        if "price" not in entry or "amount" not in entry:
+            print("Malformed Entry asks:", entry)
+            continue
+            
         price = Decimal(entry["price"])
         amount= Decimal(entry["amount"])
         asks.append([price, amount])
-        
     
     return bids, asks
+
 # simulates the api rate limit
 @rate_limiter(2)
 def fetch_coinbase ():
@@ -107,6 +115,17 @@ def exec_bid(bids, target_btc):
         total_revenue += revenue
 
     return total_revenue
+# safe decimal validator
+def positive_decimal(value):
+    try:
+        val = Decimal(value)
+    except:
+        raise argparse.ArgumentTypeError("Invalid number: '{value}'")
+    
+    if val <= 0:
+        raise argparse.ArgumentTypeError("qty must be greater than 0")
+
+    return val
 
 # Script entry point
 gemini_result = fetch_gemini()
@@ -118,10 +137,14 @@ if gemini_result is None or coinbase_result is None:
 else:
     # Read command-line argument: --qty (amount of btc)
     parser = argparse.ArgumentParser(description="BTC-USD order book aggregator")
-    parser.add_argument("--qty", type=Decimal, default=Decimal('10'), help="Quantity of BTC to buy/sell")
+    parser.add_argument("--qty", type=positive_decimal, default=Decimal('10'), help="Quantity of BTC to buy/sell")
     args = parser.parse_args()
     qty = args.qty
     
+    if qty <= 0:
+        print("Please enter a positive number")
+        exit(1)
+        
     # unpack order books
     gemini_bids, gemini_asks = gemini_result
     coinbase_bids, coinbase_asks = coinbase_result
@@ -136,6 +159,7 @@ else:
     # asks are low to high
     combined_asks.sort(key=lambda x: x[0])
     
+
     # run simulated trades
     result_ask = exec_ask(combined_asks, qty)
     result_bid = exec_bid(combined_bids,qty)
